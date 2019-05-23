84b98d45b0cacb77eac8d217c2b8d7037aa29ba5
Rhadá Costa <rhadacosta@gmail.com>
2019-05-23 17:03:48 -0300

    tela ReadBook

--- src/components/readBook/ReadBook.js
+++ src/components/readBook/ReadBook.js
@@ -2,11 +2,57 @@ import React from "react";
 import { connect } from "react-redux";
 import { readBook } from "../../actions";
 
-import Typography from "@material-ui/core/Typography";
+import './ReadBook.css'
 
 class ReadBook extends React.Component {
   render() {
-    return <dib>ReadBook</dib>;
+    return <div className="container">
+      <div className="row">
+        <div className="col-xs-12">
+          <div className="book-slide">
+          
+          <div className="book-card-detail">
+              <div className="content">
+                <h1>Capítulo 01</h1>
+                <div className="divisor"></div>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica. Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
+              </div>
+              <div className="divisorHistory"></div>
+            </div>
+            
+            <div className="book-card-detail">
+              <div className="content">
+                <h1>Capítulo 02</h1>
+                <div className="divisor"></div>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
+                  </div>
+              <div className="divisorHistory"></div>
+            </div>
+
+            <div className="book-card-detail">
+              <div className="content">
+                <h1>Capítulo 03</h1>
+                <div className="divisor"></div>
+                <p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica</p>
+                </div>
+              <div className="divisorHistory"></div>
+            </div>
+
+            <div className="choice">
+              <div className="decision one">
+                  <div className="number-choice">1</div>
+                  <p>Ação do Personagem</p>
+              </div>
+              <div className="decision two">
+                <div className="number-choice">2</div>
+                <p>Ação do Personagem</p>
+              </div>
+            </div>
+
+            </div>
+        </div>
+      </div>
+    </div>;
   }
 }
 
