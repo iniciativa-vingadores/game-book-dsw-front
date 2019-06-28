69725cac73220d680929907cf359bedaea516c10
valmirt <valmirjr_20@hotmail.com>
2019-06-27 21:07:32 -0300

    button play

--- src/components/bookDetail/BookDetail.js
+++ src/components/bookDetail/BookDetail.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import { Link } from 'react-router-dom'
-
+import { Link } from "react-router-dom";
 
 import "./BookDetail.css";
 
@@ -11,7 +10,6 @@ class BookDetail extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-xs-12 col-md-4">
-          
             <div className="book-card">
               <div className="picture-book">
                 <img alt="default" src="/book.png" />
@@ -56,18 +54,37 @@ class BookDetail extends React.Component {
             </div>
           </div>
           <div className="col-xs-12 col-md-8">
-              <h2 className="description-book-title">Descrição</h2>
-              <p className="description-book">Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.</p>
-              
-             
-              <button className="start-play">
-                  <svg id="Camada_1" data-name="Camada 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 54.02 38.01"><title>noun_Game_2532260</title><path d="M58.72,27.18,53.16,5A6.57,6.57,0,0,0,49.56.52a5.49,5.49,0,0,0-4.87.1L42,2a4.61,4.61,0,0,1-2,.49H24A4.61,4.61,0,0,1,22,2L19.32.62a5.49,5.49,0,0,0-4.87-.1A6.57,6.57,0,0,0,10.85,5L5.28,27.18a9.29,9.29,0,0,0,1.9,8.29,7.11,7.11,0,0,0,6,2.52,7.45,7.45,0,0,0,5.67-3.59l4.84-7.71a5.8,5.8,0,0,1,2.06-2,12.76,12.76,0,0,1,12.58,0,5.8,5.8,0,0,1,2.06,2l4.84,7.71A7.45,7.45,0,0,0,50.9,38h.55a7.2,7.2,0,0,0,5.37-2.52A9.29,9.29,0,0,0,58.72,27.18ZM24,15.47H20v4a1,1,0,0,1-2,0v-4H14a1,1,0,0,1,0-2h4v-4a1,1,0,1,1,2,0v4h4a1,1,0,0,1,0,2Zm20-7a2,2,0,1,1-2,2A2,2,0,0,1,44,8.47Zm-4,8a2,2,0,1,1,2-2A2,2,0,0,1,40,16.47Zm4,4a2,2,0,1,1,2-2A2,2,0,0,1,44,20.47Zm4-4a2,2,0,1,1,2-2A2,2,0,0,1,48,16.47Z" transform="translate(-4.99)"/></svg>
-                  <Link to="/books/read/1">
-                      Iniciar Jogo
-                  </Link>
-              </button>
+            <h2 className="description-book-title">Descrição</h2>
+            <p className="description-book">
+              Lorem Ipsum é simplesmente uma simulação de texto da indústria
+              tipográfica e de impressos, e vem sendo utilizado desde o século
+              XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+              os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+              sobreviveu não só a cinco séculos, como também ao salto para a
+              editoração eletrônica, permanecendo essencialmente inalterado. Se
+              popularizou na década de 60, quando a Letraset lançou decalques
+              contendo passagens de Lorem Ipsum, e mais recentemente quando
+              passou a ser integrado a softwares de editoração eletrônica como
+              Aldus PageMaker.
+            </p>
 
-              
+            <Link to="/books/read/1">
+              <button className="start-play">
+                <svg
+                  id="Camada_1"
+                  data-name="Camada 1"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 54.02 38.01"
+                >
+                  <title>noun_Game_2532260</title>
+                  <path
+                    d="M58.72,27.18,53.16,5A6.57,6.57,0,0,0,49.56.52a5.49,5.49,0,0,0-4.87.1L42,2a4.61,4.61,0,0,1-2,.49H24A4.61,4.61,0,0,1,22,2L19.32.62a5.49,5.49,0,0,0-4.87-.1A6.57,6.57,0,0,0,10.85,5L5.28,27.18a9.29,9.29,0,0,0,1.9,8.29,7.11,7.11,0,0,0,6,2.52,7.45,7.45,0,0,0,5.67-3.59l4.84-7.71a5.8,5.8,0,0,1,2.06-2,12.76,12.76,0,0,1,12.58,0,5.8,5.8,0,0,1,2.06,2l4.84,7.71A7.45,7.45,0,0,0,50.9,38h.55a7.2,7.2,0,0,0,5.37-2.52A9.29,9.29,0,0,0,58.72,27.18ZM24,15.47H20v4a1,1,0,0,1-2,0v-4H14a1,1,0,0,1,0-2h4v-4a1,1,0,1,1,2,0v4h4a1,1,0,0,1,0,2Zm20-7a2,2,0,1,1-2,2A2,2,0,0,1,44,8.47Zm-4,8a2,2,0,1,1,2-2A2,2,0,0,1,40,16.47Zm4,4a2,2,0,1,1,2-2A2,2,0,0,1,44,20.47Zm4-4a2,2,0,1,1,2-2A2,2,0,0,1,48,16.47Z"
+                    transform="translate(-4.99)"
+                  />
+                </svg>
+                Iniciar Jogo
+              </button>
+            </Link>
           </div>
         </div>
       </div>
